b43ce44945c47618d08c99ece332600adef3ee8a
sergey-poyarkin <sergey.poyarkin@gmail.com>
2023-04-23 18:43:59 +0400

    trimmed average

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         SalesManager person1 = new SalesManager(new int[]{4, 8, 12, 2, 6});
-        System.out.println(person1.max());
+        System.out.println("Max: " + person1.max());
+        System.out.println("Trimmed average: " + person1.trimAverage());
     }
 }
